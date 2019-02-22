5fc2bcadcb921d856d99027df9387da523d1a1d3
Seyyed Alireza Nouri <dalireza1374@gmail.com>
2019-02-22 19:44:23 +0330

    added Picasso dependency

--- app/src/main/java/ir/imandroid/moviesapp/activity/MainActivity.java
+++ app/src/main/java/ir/imandroid/moviesapp/activity/MainActivity.java
@@ -1,16 +1,30 @@
 package ir.imandroid.moviesapp.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ir.imandroid.moviesapp.G;
 import ir.imandroid.moviesapp.R;
 import ir.imandroid.moviesapp.api.model.GetMovies;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -20,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout lyt_movies;
     @BindView(R.id.txt_id)
     TextView txt_id;
+    @BindView(R.id.txt_title)
+    TextView txt_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +51,10 @@ public class MainActivity extends AppCompatActivity {
                 GetMovies getMovies = response.body();
                 for (GetMovies.Movie movie :getMovies.getMovies() ){
                     TextView textView = new TextView(MainActivity.this);
-                    txt_id.setText(movie.getTitle());
-//                    lyt_movies.addView(textView);
+                    ImageView imageView = new ImageView(MainActivity.this);
+
+                    txt_id.setText(String.valueOf(movie.getId()));
+                    txt_title.setText(movie.getTitle());
 
 
                 }
