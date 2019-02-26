399ad472664c3ef3104eb27f0f01f9eda46c50df
Seyyed Alireza Nouri <dalireza1374@gmail.com>
2019-02-26 16:22:25 +0330

    added animation for recyclerview items

--- app/src/main/java/ir/imandroid/moviesapp/activity/MainActivity.java
+++ app/src/main/java/ir/imandroid/moviesapp/activity/MainActivity.java
@@ -30,6 +30,8 @@ import ir.imandroid.moviesapp.G;
 import ir.imandroid.moviesapp.R;
 import ir.imandroid.moviesapp.adapter.MoviesAdapter;
 import ir.imandroid.moviesapp.api.model.GetMovies;
+import jp.wasabeef.recyclerview.animators.adapters.AlphaInAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.adapters.SlideInRightAnimationAdapter;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recycler_movies;
     List<GetMovies.Movie> movies;
     MoviesAdapter moviesAdapter;
+    SlideInRightAnimationAdapter slideInRightAnimationAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +51,11 @@ public class MainActivity extends AppCompatActivity {
         movies = new ArrayList<>();
         moviesAdapter = new MoviesAdapter(this,movies);
         recycler_movies.setLayoutManager(new GridLayoutManager(this,1, LinearLayoutManager.VERTICAL, false));
-        recycler_movies.setAdapter(moviesAdapter);
+        AlphaInAnimationAdapter alphaInAnimationAdapter = new AlphaInAnimationAdapter(moviesAdapter);
+        slideInRightAnimationAdapter = new SlideInRightAnimationAdapter(alphaInAnimationAdapter);
+        slideInRightAnimationAdapter.setDuration(1000);
+        slideInRightAnimationAdapter.setFirstOnly(false);
+        recycler_movies.setAdapter(slideInRightAnimationAdapter);
 
         Call<GetMovies> req = G.service.getMovies();
         req.enqueue(new Callback<GetMovies>() {
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<GetMovies> call, Response<GetMovies> response) {
                 GetMovies getMovies = response.body();
                 movies.addAll(getMovies.getMovies());
-                moviesAdapter.notifyDataSetChanged();
+                slideInRightAnimationAdapter.notifyDataSetChanged();
             }
 
             @Override
