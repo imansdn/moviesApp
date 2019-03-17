1d06069f6bd6634da792b57952bf903299abe9f1
Seyyed Alireza Nouri <dalireza1374@gmail.com>
2019-03-17 13:20:24 +0330

    setting animation collapsing toolbar android (not complete)

--- app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
+++ app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
@@ -1,11 +1,27 @@
 package ir.imandroid.moviesapp.activity;
 
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import ir.imandroid.moviesapp.R;
 
 public class MoreInfoActivity extends AppCompatActivity {
+
+    ImageView avatarContainerView;
+    Float expandedImageSize;
+    Float collapsedImageSize;
+    Float activityMargin;
+    Boolean valuesCalculatedAlready = false;
+    Toolbar toolbar;
+    AppBarLayout appBarLayout;
+    Float collapsedHeight = 0f;
+    Float expandedHeight = 0f;
+    Float maxOffset = 0f;
+    int lastOffset = -1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
