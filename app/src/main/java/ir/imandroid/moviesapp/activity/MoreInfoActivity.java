fda375623559d7075f0e8683e2fc3439d4394162
Seyyed Alireza Nouri <dalireza1374@gmail.com>
2019-03-18 14:47:54 +0330

    fixing CollapsingToolbarLayout (just for show, not complete for showing data)

--- app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
+++ app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
@@ -6,22 +6,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
+import java.util.List;
+
 import ir.imandroid.moviesapp.R;
 
 public class MoreInfoActivity extends AppCompatActivity {
-
-    ImageView avatarContainerView;
-    Float expandedImageSize;
-    Float collapsedImageSize;
-    Float activityMargin;
-    Boolean valuesCalculatedAlready = false;
-    Toolbar toolbar;
-    AppBarLayout appBarLayout;
-    Float collapsedHeight = 0f;
-    Float expandedHeight = 0f;
-    Float maxOffset = 0f;
-    int lastOffset = -1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
