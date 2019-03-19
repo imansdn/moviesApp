815ab225b0eb3c149d4357ac58f0597bd0b6e3f5
Imansdn <imansadrian@gmail.com>
2019-03-19 10:56:19 +0330

    remove some extra fields from constant

--- app/src/main/java/ir/imandroid/moviesapp/util/Constant.java
+++ app/src/main/java/ir/imandroid/moviesapp/util/Constant.java
@@ -2,9 +2,7 @@ package ir.imandroid.moviesapp.util;
 
 public class Constant {
 
-//    public static final String BASE_URL="http://149.202.237.114:6083";//
     public static final String BASE_URL="http://moviesapi.ir";//
-    public static final String WEBSITE_BANADER="https://bikport.pmo.ir/";
     public static final int CONNECT_TIME_OUT=60;
     public static final int READ_TIME_OUT=60;
     public static final int WRITE_TIME_OUT=60;
