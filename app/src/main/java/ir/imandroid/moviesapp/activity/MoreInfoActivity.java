cdedc29060b1a28711c5afa5f9d4588b03aa1e16
Seyyed Alireza Nouri <dalireza1374@gmail.com>
2019-03-30 12:38:19 +0430

    get data from server code

--- app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
+++ app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
@@ -8,7 +8,12 @@ import android.widget.ImageView;
 
 import java.util.List;
 
+import ir.imandroid.moviesapp.G;
 import ir.imandroid.moviesapp.R;
+import ir.imandroid.moviesapp.api.model.GetMovies;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MoreInfoActivity extends AppCompatActivity {
 
@@ -17,6 +22,19 @@ public class MoreInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
 
+        Call<GetMovies> req = G.service.getMovies();
+        req.enqueue(new Callback<GetMovies>() {
+            @Override
+            public void onResponse(Call<GetMovies> call, Response<GetMovies> response) {
+                GetMovies movieResponse = response.body();
+
+            }
+
+            @Override
+            public void onFailure(Call<GetMovies> call, Throwable t) {
+
+            }
+        });
 
     }
 }
