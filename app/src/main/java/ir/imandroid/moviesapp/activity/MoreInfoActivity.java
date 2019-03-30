1849a7d3ab8bfd175c73231080415801bdb0dd1c
Seyyed Alireza Nouri <dalireza1374@gmail.com>
2019-03-30 12:41:16 +0430

    casting imageView img_movie_poster

--- app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
+++ app/src/main/java/ir/imandroid/moviesapp/activity/MoreInfoActivity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ir.imandroid.moviesapp.G;
 import ir.imandroid.moviesapp.R;
 import ir.imandroid.moviesapp.api.model.GetMovies;
@@ -17,10 +22,13 @@ import retrofit2.Response;
 
 public class MoreInfoActivity extends AppCompatActivity {
 
+    @BindView(R.id.img_movie_poster)
+    ImageView img_movie_poster;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+        ButterKnife.bind(this);
 
         Call<GetMovies> req = G.service.getMovies();
         req.enqueue(new Callback<GetMovies>() {
